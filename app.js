c45039ad5f963efaedd48961f214e38bc47023e6
Bl4ngk <atmxtwo@gmail.com>
2023-08-29 18:00:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+let url = 'https://api.adviceslip.com/advice'
+let btn = document.querySelector('.btn');
+let txt = document.querySelector('.text');
+let adviceNum = document.querySelector('.advice__number');
+let loader = `<div class="loader"></div>`;
+
+function getData(){
+    txt.innerHTML = loader;
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET',url);
+    xhr.responseType = 'json';
+    xhr.timeout = 2000;
+    xhr.send();
+
+    xhr.onload = function() {
+        if (xhr.status != 200){
+            alert(`Error ${xhr.status}: ${xhr.statusText}`);
+        } 
+        else { 
+            updateUI(this.response.slip);
+        }
+    };
+
+    xhr.onerror = function(){
+        console.log(`Network Error`);
+    };
+}
+
+function updateUI(res){
+    txt.textContent = `"${res.advice}"`;
+    adviceNum.textContent = res.id;
+}
+
+
+document.addEventListener('DOMContentLoaded',getData);
+btn.addEventListener('click', getData);
