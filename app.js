649193e5695045456261eeea0cc2b1eb8103bdfb
Bl4ngk <atmxtwo@gmail.com>
2023-09-17 11:10:21 +0530

    changed from XML to fetch

--- app.js
+++ app.js
@@ -4,30 +4,9 @@ let txt = document.querySelector('.text');
 let adviceNum = document.querySelector('.advice__number');
 let loader = `<div class="loader"></div>`;
 
-// function getData(){
-//     txt.innerHTML = loader;
-//     let xhr = new XMLHttpRequest();
-
-//     xhr.open('GET',url);
-//     xhr.responseType = 'json';
-//     xhr.timeout = 2000;
-//     xhr.send();
-
-//     xhr.onload = function() {
-//         if (xhr.status != 200){
-//             alert(`Error ${xhr.status}: ${xhr.statusText}`);
-//         } 
-//         else { 
-//             updateUI(this.response.slip);
-//         }
-//     };
-
-//     xhr.onerror = function(){
-//         console.log(`Network Error`);
-//     };
-// }
 
 function getData(){
+    txt.innerHTML = loader;
     fetch(url)
         .then((resp) => {
             if(!resp.ok) throw new Error('Opps');
