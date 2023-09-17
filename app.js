b4e3d05e16b4dc811fe69616004c713c3a743eb2
Bl4ngk <atmxtwo@gmail.com>
2023-09-17 11:04:03 +0530

    bug fix

--- app.js
+++ app.js
@@ -4,27 +4,42 @@ let txt = document.querySelector('.text');
 let adviceNum = document.querySelector('.advice__number');
 let loader = `<div class="loader"></div>`;
 
+// function getData(){
+//     txt.innerHTML = loader;
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.open('GET',url);
+//     xhr.responseType = 'json';
+//     xhr.timeout = 2000;
+//     xhr.send();
+
+//     xhr.onload = function() {
+//         if (xhr.status != 200){
+//             alert(`Error ${xhr.status}: ${xhr.statusText}`);
+//         } 
+//         else { 
+//             updateUI(this.response.slip);
+//         }
+//     };
+
+//     xhr.onerror = function(){
+//         console.log(`Network Error`);
+//     };
+// }
+
 function getData(){
-    txt.innerHTML = loader;
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('GET',url);
-    xhr.responseType = 'json';
-    xhr.timeout = 2000;
-    xhr.send();
-
-    xhr.onload = function() {
-        if (xhr.status != 200){
-            alert(`Error ${xhr.status}: ${xhr.statusText}`);
-        } 
-        else { 
-            updateUI(this.response.slip);
-        }
-    };
-
-    xhr.onerror = function(){
-        console.log(`Network Error`);
-    };
+    fetch(url)
+        .then((resp) => {
+            if(!resp.ok) throw new Error('Opps');
+            return resp.json();
+        })
+        .then((dataobj) =>{
+            console.log(dataobj);
+            updateUI(dataobj.slip);
+        })
+        .catch((err) => {
+            console.warn(err.message);
+        });
 }
 
 function updateUI(res){
